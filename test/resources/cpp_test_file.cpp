4eac07f68b0505c6734f120cfee656fcb7a9142a
Yanay Goor <yanay.goor@gmail.com>
2020-02-08 15:12:34 +0200

    added class to test cpp code

--- test/resources/cpp_test_file.cpp
+++ test/resources/cpp_test_file.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
 
 struct my_struct {
     int bla;
     char *stuff;
+};
+
+class MyClass {
+    public:
+        int bla;
+        std::string stuff;
+        MyClass(int bla, const std::string stuff) {
+            this->bla = bla;
+            this->stuff = stuff;
+            std::cout << "New MyClass!" << std::endl;
+        }
 };
 
 
@@ -13,6 +25,8 @@ struct my_struct do_stuff(int my_arg, char *second_arg)
 
     for (i = 0; i < my_local; ++i)
         std::cout << "i = " << i << std::endl;
+
+    MyClass my_obj(6, "6");
 
     struct my_struct a = {.bla=5, .stuff="5"};
     return a;
